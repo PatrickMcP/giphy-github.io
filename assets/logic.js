ac6d4be505798480c47dab2a1d8211357065c11b
Patrick McPartland <patrick.c.mcpartland@gmail.com>
2018-09-09 16:07:33 -0600

    fixed everything but not loading gifs after adding to button array

--- assets/logic.js
+++ assets/logic.js
@@ -1,76 +1,81 @@
 var gameList = ["Divinity Original Sin 2", "Final Fantasy VII", "Final Fantasy IX", "World of Warcraft", "EverQuest", "Dark Souls", "Bloodborne"];
 
 function renderButtons() {
-    $("#buttonDiv").empty();
+  $("#buttonDiv").empty();
+  for (var i = 0; i < gameList.length; i++) {
+    var gameButton = $("<button>");
+    gameButton.addClass("game");
+    gameButton.addClass("btn btn-info mx-2 text-dark my-1");
+    gameButton.attr("data-name", gameList[i]);
     
-    for (var i = 0; i < gameList.length; i++) {
-        var a = $("<button>");
-        // Adding a class
-        a.addClass("game");
-        // Adding a data-attribute with a value of the movie at index i
-        a.attr("data-name", gameList[i]);
-        // Providing the button's text with a value of the movie at index i
-        a.text(gameList[i]);
-        // Adding the button to the HTML
-        $("#buttonDiv").append(a);
-    }
+    gameButton.text(gameList[i]);
+    
+    $("#buttonDiv").append(gameButton);
+  }
 };
-
-$("#game").on("click", function(event) {
-    event.preventDefault();
-
-    var game = $("#game-input").val().trim();
-    
-    gameList.push(game);
-
-    renderButtons();
-  });
 
 renderButtons();
 
-$("button").on("click", function() {
-    event.preventDefault();
-    $("#gif-view").html("");
-    var gameName = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gameName + "&api_key=XJ8F9R1buQngpS44OJSMisUiYi7yzYGl&limit=10";
+$("button").on("click", function () {
+  event.preventDefault();
 
-    console.log(queryURL);
+  $("#gif-view").html("");
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-        var results = response.data;
+  var gameName = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gameName + "&api_key=XJ8F9R1buQngpS44OJSMisUiYi7yzYGl&limit=10";
 
-        // Looping over every result item
-        for (var i = 0; i < results.length; i++) {
+  console.log(queryURL);
 
-          // Only taking action if the photo has an appropriate rating
-          if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-            // Creating a div with the class "item"
-            var gifDiv = $("<div class='col-md-4'>");
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    var results = response.data;
 
-            // Storing the result item's rating
-            var rating = results[i].rating;
+    for (var i = 0; i < results.length; i++) {
 
-            // Creating a paragraph tag with the result item's rating
-            var p = $("<p>").text("Rating: " + rating);
+      if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+    
+        var gifDiv = $("<div class='col-md-4'>");
 
-            // Creating an image tag
-            var personImage = $("<img>");
+        var rating = results[i].rating;
 
-            // Giving the image tag an src attribute of a proprty pulled off the
-            // result item
-            personImage.attr("src", results[i].images.fixed_height.url);
+        var p = $("<p>").text("Rating: " + rating);
 
-            // Appending the paragraph and personImage we created to the "gifDiv" div we created
-            gifDiv.append(p);
-            gifDiv.append(personImage);
+        var gameImage = $("<img>");
 
-            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-            $("#gif-view").prepend(gifDiv);
-          }
-        }
-      });
+        gameImage.attr("src", results[i].images.fixed_height_small_still.url); // still image stored into src of image
+        gameImage.attr("data-still",results[i].images.fixed_height_small_still.url); // still image
+        gameImage.attr("data-animate",results[i].images.fixed_height_small.url); // animated image
+        gameImage.attr("data-state", "still"); // set the image state
+        gameImage.addClass("image");
 
-    });
+        gifDiv.append(p);
+        gifDiv.append(gameImage);
+
+        $("#gif-view").prepend(gifDiv);
+      }
+    }
+  });
+});
+
+$("#add-game").on("click", function () {
+  event.preventDefault();
+
+  var game = $("#game-input").val().trim();
+
+  gameList.push(game);
+
+  renderButtons();
+});
+
+$(document).on("click", ".image", function(){
+  var state = $(this).attr('data-state');
+  if ( state == 'still'){
+      $(this).attr('src', $(this).data('animate'));
+      $(this).attr('data-state', 'animate');
+  }else{
+      $(this).attr('src', $(this).data('still'));
+      $(this).attr('data-state', 'still');
+  }
+});
